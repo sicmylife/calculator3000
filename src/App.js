32ae288bbb7c6c20ad206f562dc18af478db3095
Daniel <sicmylife@gmail.com>
2022-10-01 15:52:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import { useState} from 'react';
+import { evaluate } from 'mathjs';
+
+
+
+function App() {
+
+const [input,setInput] = useState('');
+
+const agregarInput = valor => {
+  setInput(input + valor);
+};
+
+const calcularResultado = () => {
+  if (input) {
+    setInput(evaluate(input));
+  } else {
+    alert("Ingrese Valores")
+  }
+  
+}
+
+
+
+  return (
+    <div className="App">
+
+    <div className='logo-contenedor'>
+  
+         <span className='name'>Code React JS by Daniel Daso</span>
+    </div>
+
+    <div className='contenedor-calculadora'>
+      <Pantalla input={input} />
+    <div className='fila'>
+      <Boton  manejarClic={agregarInput}>1</Boton>
+      <Boton manejarClic={agregarInput}>2</Boton>
+      <Boton manejarClic={agregarInput}>3</Boton>
+      <Boton manejarClic={agregarInput}>+</Boton>
+    </div>
+    <div className='fila'>
+      <Boton manejarClic={agregarInput}>4</Boton>
+      <Boton manejarClic={agregarInput}>5</Boton>
+      <Boton manejarClic={agregarInput}>6</Boton>
+      <Boton manejarClic={agregarInput}>-</Boton>
+    </div>
+    <div className='fila'>
+    <Boton>7</Boton>
+      <Boton manejarClic={agregarInput}>8</Boton>
+      <Boton manejarClic={agregarInput}>9</Boton>
+      <Boton manejarClic={agregarInput}>*</Boton>
+    </div>
+    <div className='fila'>
+      <Boton manejarClic={calcularResultado}>=</Boton>
+      <Boton manejarClic={agregarInput}>0</Boton>
+      <Boton manejarClic={agregarInput}>.</Boton>
+      <Boton manejarClic={agregarInput}>/</Boton>
+    </div>
+    <div className='fila'>
+      <BotonClear manejarClear={()=> setInput('')}>Clear</BotonClear>
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
